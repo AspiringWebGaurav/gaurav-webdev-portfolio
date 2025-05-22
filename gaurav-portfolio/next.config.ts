0711265fdb317c49bc85b39620a034cd9e747b4b
AspiringWebGaurav <gauravpatil5737@gmail.com>
2025-05-22 15:06:19 +0530

    fix: netlify automated deploy and stable website

--- gaurav-portfolio/next.config.ts
+++ gaurav-portfolio/next.config.ts
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  // Disable linting during build
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    domains: ["avatars.githubusercontent.com", "media.graphassets.com"],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
