c2d9c3d43fd5d234ab5330dc7a21ac7065eae627
AspiringWebGaurav <gauravpatil5737@gmail.com>
2025-05-23 12:46:24 +0530

    animated Project Card added and stable and functional

--- gaurav-portfolio/app/page.tsx
+++ gaurav-portfolio/app/page.tsx
@@ -1,21 +1,20 @@
 import Hero from "@/components/Hero";
-import { FloatingNav } from './../components/ui/FloatingNav';
-import { FaHome } from 'react-icons/fa';
+import { FloatingNav } from "./../components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
-
-
-
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div>
         <h1 className="text-white max-w-7xl w-full">
-          < FloatingNav navItems = {[
-            {name:'Home', link: '/', icon: <FaHome />}
-          ]} />
+          <FloatingNav
+            navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+          />
           <Hero />
           <Grid />
+          <RecentProjects />
         </h1>
       </div>
     </main>
