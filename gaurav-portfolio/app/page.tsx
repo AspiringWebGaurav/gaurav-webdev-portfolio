21fd27c2b8420b6a9abd549cd3bdd9b3173a5127
AspiringWebGaurav <gauravpatil5737@gmail.com>
2025-05-23 19:11:19 +0530

    all website v1 completed

--- gaurav-portfolio/app/page.tsx
+++ gaurav-portfolio/app/page.tsx
@@ -6,10 +6,12 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div>
         <h1 className="text-white max-w-7xl w-full">
           <FloatingNav navItems={navItems} />
@@ -18,6 +20,8 @@ export default function Home() {
           <RecentProjects />
           <Clients />
           <Experience />
+          <Approach />
+          <Footer />
         </h1>
       </div>
     </main>
