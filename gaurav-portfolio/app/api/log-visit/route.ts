ae4b36100c87857a29f4618675165b34c06af1e3
AspiringWebGaurav <gauravpatil5737@gmail.com>
2025-06-02 10:47:24 +0530

    working on bug

--- gaurav-portfolio/app/api/log-visit/route.ts
+++ gaurav-portfolio/app/api/log-visit/route.ts
@@ -1,6 +1,6 @@
 // app/api/log-visit/route.ts
 import { NextRequest, NextResponse } from "next/server";
-import { db } from "@/utils/firebase"; // ✅ correct path to firebase.js
+import { db } from "@/lib/firebase"; // ✅ correct path to firebase.js
 import {
   collection,
   addDoc,
