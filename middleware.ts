ffd514daf3fbe7b929c12bdbaaa0ab416787fc94
Gaurav Patil <gauravpatil5737@gmail.com>
2026-09-20 19:27:09 +0530

    perf(vercel): optimize middleware quota usage

--- middleware.ts
+++ middleware.ts
@@ -89,7 +89,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/admin", "/admin/:path*"],
 };
 
 
